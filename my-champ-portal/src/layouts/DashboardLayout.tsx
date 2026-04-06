76e0873fb1071ed3e28950aef9bf703616b083d5
Stephanie Correll <scorrell@cleverbenefits.com>
2026-04-06 17:15:01 -0400

    Add ErrorBoundary to DashboardLayout for improved error handling around Outlet component. This change enhances user experience by providing a fallback UI in case of rendering errors.

--- my-champ-portal/src/layouts/DashboardLayout.tsx
+++ my-champ-portal/src/layouts/DashboardLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from '../components/layout/Sidebar'
 import { ToastProvider } from '../components/feedback/Toast'
+import { ErrorBoundary } from '../components/feedback/ErrorBoundary'
 
 export function DashboardLayout() {
   return (
@@ -8,7 +9,9 @@ export function DashboardLayout() {
       <Sidebar />
       <main className="flex-1 overflow-auto">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <Outlet />
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
         </div>
       </main>
       <ToastProvider />
